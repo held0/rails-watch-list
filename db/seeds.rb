a9af4e75fc45cb808cd597f985a765fdae1bf3d3
Niklas Held <niklas.held@gmail.com>
2022-11-18 11:23:40 +0100

    changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,4 +14,3 @@ movie_data['results'].each do |movie|
   Movie.create(title: movie['original_title'], overview: movie['overview'],
                poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}", rating: movie['vote_average'])
 end
-puts Movie.all.length
